8f0ccf97a1c41bb31d259cac5d3f85495da8630c
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2026-03-02 19:50:24 -0500

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1597,7 +1597,13 @@ static void ThreadMapPort()
     struct IGDdatas data;
     int r;
 
-    r = UPNP_GetValidIGD(devlist, &urls, &data, lanaddr, sizeof(lanaddr));
+    // r = UPNP_GetValidIGD(devlist, &urls, &data, lanaddr, sizeof(lanaddr));
+    #if (defined(MINIUPNPC_API_VERSION) && MINIUPNPC_API_VERSION >= 14) || \
+        (defined(UPNP_VERSION) && UPNP_VERSION >= 18)
+        r = UPNP_GetValidIGD(devlist, &urls, &data, lanaddr, sizeof(lanaddr), NULL, 0);
+    #else
+        r = UPNP_GetValidIGD(devlist, &urls, &data, lanaddr, sizeof(lanaddr));
+    #endif
     if (r == 1)
     {
         if (fDiscover) {
